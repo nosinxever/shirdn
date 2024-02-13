1debb1486ba3f79ebaf33f3e80db48ce6e6442be
nosinxever <nosinxever@gmail.com>
2024-02-14 06:22:29 +0800

    add footer

---
+++ src/app/components/Footer.js
@@ -0,0 +1,9 @@
+// components/Footer.js
+export default function Footer() {
+    return (
+      <footer className="bg-pink-200 text-white text-center p-4">
+        <p>Evan with ❤️ 2024 </p>
+      </footer>
+    );
+  }
+  
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import ValentinesCard from "./components/ValentinesCard";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
+    <>
     <div className="flex justify-center items-center h-screen bg-pink-50">
       <ValentinesCard />
     </div>
+    <Footer />
+    </>
+    
   );
 }
