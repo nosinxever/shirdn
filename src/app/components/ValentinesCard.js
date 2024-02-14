3a83d9ff7cd64d691c6e343f9b034a6c400baacb
nosinxever <nosinxever@gmail.com>
2024-02-14 10:57:22 +0800

    update

--- src/app/components/ValentinesCard.js
+++ src/app/components/ValentinesCard.js
@@ -1,15 +1,33 @@
 // components/ValentinesCard.js
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 export default function ValentinesCard() {
-    return (
-      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-pink-100 m-4 p-4">
-        <img className="w-full" src="/valentines-day.jpg" alt="Valentine's Day" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2 text-pink-700">Happy Valentine's Day!</div>
-          <p className="text-gray-700 text-base">
-            Wishing you a day filled with love and happiness! 💖
-          </p>
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-pink-100 m-4 p-4">
+      <img className="w-full" src="/valentines-day.jpg" alt="Valentine's Day" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2 text-pink-700">
+          Happy Valentine's Day!
+        </div>
+        <p className="text-gray-700 text-base pb-5">
+          Wishing you a day filled with love and happiness! 💖
+        </p>
+        <div className="flex ">
+          <div className="pr-10">
+            <Avatar>
+              <AvatarImage src="/Shirdn.jpg" alt="@shadcn" />
+              <AvatarFallback>XD</AvatarFallback>
+            </Avatar> 
+          </div>
+          <div >
+            <Avatar>
+              <AvatarImage src="/Evan.jpg" alt="@shadcn" />
+              <AvatarFallback>SJ</AvatarFallback>
+            </Avatar>
+          </div>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Happy valentine's day! 💖 shirden 💖",
-  description: "Happy valentine's day! 💖 shirden 💖, by Evan Feb 14 2024",
+  title: "💖 Shirdn 💖 Happy Valentine's Day! 💖",
+  description: "Happy Valentine's Day! 💖 shirdn 💖, by Evan Feb 14 2024",
 };
 
 export default function RootLayout({ children }) {
