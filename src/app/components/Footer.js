57fb881d3e15482d873e704ec0e8dd8c8e8fe884
nosinxever <nosinxever@gmail.com>
2024-02-14 13:06:26 +0800

    update

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,7 +2,7 @@
 export default function Footer() {
     return (
       <footer className="bg-pink-200 text-white text-center p-4">
-        <p>By Evan with ❤️ 2024 </p>
+        <p>By lishujun with ❤️ 2024 </p>
       </footer>
     );
   }
