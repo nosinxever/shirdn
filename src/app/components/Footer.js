f29047a76a8c3ad1ddf727d6253fdfb2f11c0ada
nosinxever <nosinxever@gmail.com>
2024-02-14 14:14:38 +0800

    update

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,7 +2,7 @@
 export default function Footer() {
     return (
       <footer className="bg-pink-200 text-white text-center p-4">
-        <p>By lishujun with ❤️ 2024 </p>
+        <p>By Li Shujun with ❤️ 2024 </p>
       </footer>
     );
   }
--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
     <div className="flex flex-col	 items-center h-screen bg-pink-50">
-      <p className="font-bold text-2xl my-5 text-pink-700 justify-center items-center"> Happy Valentine's Day!</p>
+      <p className="font-bold text-2xl my-8 text-pink-700 justify-center items-center"> Happy Valentine's Day!</p>
 
       <ValentinesCard />
     </div>
