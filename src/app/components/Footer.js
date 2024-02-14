24b1de600fb39cba7b149981bb19211ff1808ccd
nosinxever <nosinxever@gmail.com>
2024-02-14 15:12:28 +0800

    update footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,7 +2,8 @@
 export default function Footer() {
     return (
       <footer className="bg-pink-200 text-white text-center p-4">
-        <p>Li Shujun with ❤️ 2024 </p>
+        <p> 胡雪丹，我可以追你吗？ </p>
+        <p>李树君 with ❤️ 2024 </p>
       </footer>
     );
   }
