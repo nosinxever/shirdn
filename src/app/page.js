00d81b4b0ad83c7a79cf6ee1bdc6998564fa3441
nosinxever <nosinxever@gmail.com>
2024-02-14 13:57:28 +0800

    update

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,9 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <>
-    <div className="flex justify-center items-center h-screen bg-pink-50">
+    <div className="flex flex-col	justify-center items-center h-screen bg-pink-50">
+      <p className="font-bold text-2xl mb-5 text-pink-700 justify-center items-center"> Happy Valentine's Day!</p>
+
       <ValentinesCard />
     </div>
     <Footer />
