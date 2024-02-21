8de4eb300880893114ae712f38d1430f575a6e66
nosinxever <nosinxever@gmail.com>
2024-02-21 15:21:07 +0800

    update time zone

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -10,15 +10,20 @@ export function convertTimeFormat(dateTimeString) {
   // Create a new Date object using the provided date-time string
   const date = new Date(dateTimeString);
 
-  // e.g., "January 1, 2024, 14:32:00"
-  const readableFormat = date.toLocaleString('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-  });
+  // Specify the options for formatting, including the time zone for China
+  const options = {
+    timeZone: 'Asia/Shanghai', // Specify the time zone for China
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: false // Optional: set to false to use 24-hour time format
+  };
+
+  // Convert the date to a readable format in China's time zone
+  const readableFormat = date.toLocaleString('en-US', options);
   
   return readableFormat;
 }
