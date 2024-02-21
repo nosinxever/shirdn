13ce3fb4651ab0c3187f67a9badb08e37f975120
nosinxever <nosinxever@gmail.com>
2024-02-21 14:59:49 +0800

    update

---
+++ src/app/shirdn/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import AddWish from "@/components/AddWish";
+import CreateDatabase from "@/components/CreateDatabase";
+
+export default function Admin() {
+  return (
+    <div className="flex flex-col md:flex-row md:space-x-8 p-4 space-y-8 md:space-y-0 w-full max-w-4xl mx-auto">
+      {/* <CreateDatabase /> */}
+      <AddWish />
+    </div>
+  );
+}
