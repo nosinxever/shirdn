1ba1d276370373afbe88d1dbbf0d519595cf84c2
nosinxever <nosinxever@gmail.com>
2024-02-22 16:18:35 +0800

    update

---
+++ src/app/emotions/page.js
@@ -0,0 +1,30 @@
+import WishList from "@/components/WishList";
+
+export const dynamic = 'force-dynamic'
+
+export default async function Home() {
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/wishs`);
+  
+  if (!response.ok) {
+    console.log(new Error(`HTTP error! when fetching /api/wishs, status: ${response.status}`)) 
+    return (
+      <div>
+        <p>HTTP error! when fetching /api/wishs </p>
+        {response.status}
+      </div>
+    )
+  }
+  
+  const wishs = await response.json();
+  
+  return (
+    <div >
+      <WishList wishs={wishs} />
+    </div>
+  )
+}
+
+
+
+
