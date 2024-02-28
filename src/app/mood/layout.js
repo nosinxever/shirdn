b1d6780295e42a6e1278ac4c1aa977face453124
nosinxever <nosinxever@gmail.com>
2024-02-28 12:39:38 +0800

    update

---
+++ src/app/mood/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: " Shirdn's mood 💖",
+    description: "Shirdn's mood 💖",
+  };
+
+
+  export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <body >{children}</body>
+      </html>
+    );
+  }
+  
