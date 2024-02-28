0832580098088e2e6acc7289bfc6a46f8b78531c
nosinxever <nosinxever@gmail.com>
2024-02-28 09:49:50 +0800

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="absolute bottom-0 w-full text-center pb-4 text-gray-600">
+        <footer className="absolute bottom-0 w-full text-center pb-4 text-gray-600 mt-3">
         Made with <span className="text-red-500">♥</span> for the love of my life
       </footer>
     );
