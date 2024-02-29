703399c9af2ff3e6409aa78721e371c46c407e67
nosinxever <nosinxever@gmail.com>
2024-02-29 12:27:53 +0800

    update

--- src/app/mood/page.js
+++ src/app/mood/page.js
@@ -2,39 +2,7 @@
 
 "use client"
 
-// import AddMood from "@/components/AddMood";
-// import MoodList from "@/components/MoodList";
-// import Footer from "@/components/Footer";
-
 export const dynamic = 'force-dynamic'
-
-// export default async function Home() {
-
-//   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/mood`);
-
-//   if (!response.ok) {
-//     console.log(new Error(`HTTP error! when fetching /api/mood, status: ${response.status}`)) 
-//     return (
-//       <div>
-//         <p>HTTP error! when fetching /api/mood </p>
-//         {response.status}
-//       </div>
-//     )
-//   }
-  
-//   const moods = await response.json();
-
-//   return (
-//     <div className="mx-3 ">
-//       <AddMood />
-//       <MoodList moods={moods}/>
-//     </div>
-
-//   )
-// }
-
-
-
 
 import { useState, useEffect } from "react";
 import AddMood from "@/components/AddMood";
--- src/components/AddMood.js
+++ src/components/AddMood.js
@@ -1,68 +1,7 @@
+//AddMood.js
+
 "use client"
-
-// import { useState } from "react";
-
-// const AddMood = () => {
-//   const [title, setTitle] = useState("");
-
-//   const [message, setMessage] = useState("");
-//   const [isSubmitting, setIsSubmitting] = useState(false); // To disable the button during submission
-
-//   // Function to check if the form has been modified (to enable the submit button)
-//   const isFormModified = () => title !== "";
-
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     setIsSubmitting(true); // Disable the button to prevent multiple submissions
-//     const response = await fetch('/api/mood', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify({ title}),
-//     });
-
-//     const data = await response.json();
-
-//     if (!response.ok) {
-//       setIsSubmitting(false); // Re-enable the button in case of an error
-//       throw new Error(`HTTP error! status: ${response.status}`);
-//     } else {
-//       // Clear inputs and show the message
-//       setTitle("");
-//       setMessage(`${data.message}, ${data.postId}`);
-//       setIsSubmitting(false); // Re-enable the button after successful submission
-//     }
-//   };
-
-//   return (
-//     <div className="mx-auto w-full my-5 md:w-96">
-//     <form onSubmit={handleSubmit} className="flex items-end space-x-4">
-//       <textarea
-//         type="text"
-//         className="w-full p-2 border border-gray-200 shadow-md rounded-md focus:border-gray-100 focus:ring-0"
-//         placeholder="请尽情表达你的不满和满意。。。"
-//         value={title}
-//         onChange={(e) => setTitle(e.target.value)}
-//         style={{ height: '72px',resize: 'none' }} // Set fixed height for textarea
-
-//       />
-//       <button
-//         type="submit"
-//         disabled={!isFormModified() || isSubmitting} 
-//         className="flex items-center justify-center p-3 rounded-md hover:bg-blue-300"
-//         style={{ height: '72px' }} // Ensure button height matches textarea
-//       >
-//       <SvgIcon />
-//       </button>
-//     </form>
-//     {/* {message && <p className="text-green-600">{message}</p>} */}
-//   </div>
-   
-//   );
-// };
-
-
-
+import { useState } from "react";
 
 const SvgIcon = () => (
     <svg className="svg-icon" style={{width: '24px', height: '100%', verticalAlign: 'middle', fill: 'currentColor', overflow: 'hidden'}} viewBox="0 0 1126 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
@@ -71,7 +10,6 @@ const SvgIcon = () => (
   );
 
 
-import { useState } from "react";
 
 const AddMood = ({ onNewMoodAdded }) => {
   const [title, setTitle] = useState("");
